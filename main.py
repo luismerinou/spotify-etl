025413409fa21cd6c8858c9d9f0651db9b3bf7df
Luis Merino Ulizarna <luismuli@ext.inditex.com>
2024-03-08 17:37:19 +0100

    feat: load process with data validations

--- main.py
+++ main.py
@@ -17,6 +17,34 @@ def get_recently_played_after_time(my_time, headers):
         headers=headers)
 
 
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False
+
+        # Primary Key Check
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key check is violated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    # Check that all timestamps are of yesterday's date
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+            raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
+
+    return True
+
+
 if __name__ == "__main__":
 
     headers = {
@@ -31,8 +59,10 @@ if __name__ == "__main__":
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
     # Download all songs you've listened to "after yesterday", which means in the last 24 hours
+    print(" ####### Extracting your masterpieces from Spotify... #######")
     request = get_recently_played_after_time(yesterday, headers)
     response = request.json()
+    print(" ####### Extracted! Take a look to your last songs! #######")
 
     song_names = []
     artist_names = []
@@ -56,3 +86,6 @@ if __name__ == "__main__":
 
     song_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
     print(song_df)
+
+    if check_if_valid_data(song_df):
+        print("####### Data valid, proceed to Load stage #######")
