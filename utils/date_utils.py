64b5660645937fa4716f30586861a13baa3a0022
Luis Merino Ulizarna <luismuli@ext.inditex.com>
2024-03-20 18:35:26 +0100

    chore: add utils

---
+++ utils/date_utils.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+
+
+def get_today() -> datetime:
+    return datetime.now()
+
+
+def get_yesterday() -> datetime:
+    yesterday = get_today() - timedelta(days=1)
+    return yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
---
+++ utils/transforms.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+import pandas as pd
+from utils.date_utils import get_yesterday, get_today
+
+
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False
+
+    if not pd.Series(df['played_at']).is_unique:
+        raise KeyError(f"Primaray Key is vioalated: {df['played_at']} is duplicated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception(f"Null values found: {df.isnull().values}")
+
+    timestamps: list = df["timestamp"].tolist()
+    not_yesterday: list = []
+    for timestamp in timestamps:
+        if datetime.strptime(timestamp, '%Y-%m-%d') != get_yesterday():
+            not_yesterday.append(timestamp)
+
+    return is_empty(not_yesterday)
+
+
+def is_empty(my_list: list) -> bool:
+    return len(my_list) > 0
