78ddeb638b95809affcdce39d4efae4ae506d6aa
Luis Merino Ulizarna <luismuli@ext.inditex.com>
2024-03-20 20:00:56 +0100

    chore: add method

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -8,3 +8,7 @@ def get_today() -> datetime:
 def get_yesterday() -> datetime:
     yesterday = get_today() - timedelta(days=1)
     return yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def get_yesterday_unix() -> int:
+    return int(get_yesterday().timestamp()) * 1000
