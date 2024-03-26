1dec617e1e084d1e10022927a12af0dbd5b51909
Luis Merino Ulizarna <luismuli@ext.inditex.com>
2024-03-26 21:03:25 +0100

    feat: add human dates

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -5,9 +5,17 @@ def get_today() -> datetime:
     return datetime.now()
 
 
+def get_today_human() -> str:
+    return datetime.now().strftime("%A, %B %d")
+
+
 def get_yesterday() -> datetime:
     yesterday = get_today() - timedelta(days=1)
     return yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def get_yesterday_human() -> str:
+    return get_yesterday().strftime("%A, %B %d")
 
 
 def get_yesterday_unix() -> int:
