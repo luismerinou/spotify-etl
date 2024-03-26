577c9c19c2edeb12778b4d39fe85a1e8d154961e
Luis Merino Ulizarna <luismuli@ext.inditex.com>
2024-03-26 21:03:50 +0100

    chore: refactor main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+from datetime import datetime
 
 import pandas as pd
 
@@ -8,8 +9,9 @@ from etl.transforms import check_if_valid_data
 from dotenv import load_dotenv
 
 from utils.api import get_recently_played_after_time
-from utils.date_utils import get_yesterday_unix
+from utils.date_utils import get_yesterday_unix, get_today, get_yesterday, get_today_human, get_yesterday_human
 from etl.load import load_sqlLite
+from etl.extract import extract_from_api
 
 
 if __name__ == "__main__":
@@ -21,37 +23,16 @@ if __name__ == "__main__":
 
     print(" ####### Extracted! Take a look to your last songs! #######")
 
-    song_names = []
-    artist_names = []
-    played_at_list = []
-    timestamps = []
-
-    try:
-        # Relevant fields only
-        for song in response["items"]:
-            song_names.append(song["track"]["name"])
-            artist_names.append(song["track"]["album"]["artists"][0]["name"])
-            played_at_list.append(song["played_at"])
-            timestamps.append(song["played_at"][0:10])
-    except KeyError:
-        raise KeyError("Items not found, check if your Spotify Access Token has expired and retry")
-
-    # Prepare a dictionary in order to turn it into a pandas dataframe below
-    song_dict = {
-        "song_name": song_names,
-        "artist_name": artist_names,
-        "played_at": played_at_list,
-        "timestamp": timestamps
-    }
-
-    song_df = pd.DataFrame(song_dict, columns=["song_name", "artist_name", "played_at", "timestamp"])
+    song_df = extract_from_api(response)
     print(song_df)
 
     if check_if_valid_data(song_df):
         print("####### Data valid, proceed to Load stage #######")
         load_sqlLite(song_df, to_s3=True)
     else:
-        print("Invalid data")
+        raise Exception(
+            "####### Data invalid, probably not containing data only from yesterday {}\n but today is {}, not loaded to any "
+            "data storage system #######".format(get_yesterday_human(), get_today_human()))
 
 
 
