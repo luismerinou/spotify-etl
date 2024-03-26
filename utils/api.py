9d42c3418b6779baca0043c6cee1379abcb77e2e
Luis Merino Ulizarna <luismuli@ext.inditex.com>
2024-03-26 20:43:15 +0100

    chore: refactor api.py

--- utils/api.py
+++ utils/api.py
@@ -1,30 +1,19 @@
 import os
 
 import requests
+# from utils.auth import get_token
 
 
 def get_recently_played_after_time(unix_date: int) -> requests.Response:
-    TOKEN = ("BQB-AQcVxuZ1-ls_EIc1OwPnjF4j1sJPa5fhujvrHEJcOvqhEnZlQmCbrikXc2gAStCDFkEEH5_40WgTV6NbcOe-4RXYgh7-Tab"
-             "-vftocY15zY-aMtsE0eb3lrPAiUfLJi_jHL20GpXWZIa_tv09sOEevWGt3nBwitoY1SPjlvdU7s6Z3atRN35CDcrUjfOZ0LFvdu_5D4CmQsOyGoDpus47-WK5yF_Y7XnP9nUEs4ESTQS78MvZmaO8jQ")
+    # token = get_token()
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
-        "Authorization": "Bearer {token}".format(token=get_token())
+        "Authorization": "Bearer {token}".format(token=os.getenv("TOKEN"))
     }
-    try:
-        response: requests.Response = requests.get(
-            "https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=unix_date),
-            headers=headers).json()
-        return response
-    except requests.exceptions.HTTPError as err:
-        if err == 401:  # Token expired
-            Exception("Token expired. Refresh it and retry")
-        else:
-            print(f"HTTP Error: {err}")
-    except Exception as e:
-        # Handle other exceptions
-        Exception(f"An error occurred: {e}")
+    response: requests.Response = requests.get(
+        "https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=unix_date),
+        headers=headers).json()
 
+    return response
 
-def get_token() -> str:
-    return os.getenv("TOKEN")
